1fdf8c0390e8d239aed3908f63c3d6524fef1549
helmelix <ilakisov@gmail.com>
2016-10-25 23:41:46 +0300

    some cleanup

--- libs/routes/routes.js
+++ libs/routes/routes.js
@@ -22,29 +22,24 @@ var checkToken = function (req, res, next) {
 
 			hostname: authIp.ip,
             path: '/api/autorize',
-			  //forever : true,
               port: authIp.port,
               method: 'GET',
                 headers: {
 				'Content-Type': 'application/x-www-form-urlencoded',
 				'authorization': req.headers['authorization']
-				//'Content-Length': postData.length
+
 			  }
 	}
 	console.log(util.inspect(options, {showHidden: false, depth: null}));
 	var authRequest = http.request(options, function(response) {
 	  console.log('STATUS: ' + response.statusCode);
-	  //console.log('HEADERS: ' + JSON.stringify(response.headers));
-	  //res.setEncoding('utf8');
 
 		response.on('data', function (chunk) {
 
 			if(response.statusCode===200){
 				//var data = JSON.stringify(chunk);
 				var data = JSON.parse(chunk);
-				//console.log('BODY: ' + data);
-				//console.log('response on data' + chunk);
-				//req.user = chunk.user;
+
 				req.user = data.user;
 				next();
 			}else{
@@ -99,15 +94,9 @@ router.get('/',  function(req, res) {
 		if(!route) {
 			res.statusCode = 404;
 			res.end();
-			//return res.json({ 
-			//	error: 'Not found' 
-			//});
 		}
 		
 		if (!err) {
-			
-
-
 			
 			return res.json({ 
 				'data': outData.routesToJsonV_1(route) 
@@ -116,10 +105,7 @@ router.get('/',  function(req, res) {
 			res.statusCode = 500;
 			res.end();
 			log.error('Internal error(%d): %s',res.statusCode,err.message);
-			
-			//return res.json({ 
-			//	error: 'Server error' 
-			//});
+
 		}
 	});
 });
@@ -153,50 +139,12 @@ router.get('/:id',  function(req, res) {
 });
 
 
-/*
-router.get('/', passport.authenticate('bearer', { session: false }), function(req, res) {
-	
-	DriveRoute.find(function (err, articles) {
-		if (!err) {
-			return res.json(articles);
-		} else {
-			res.statusCode = 500;
-			
-			log.error('Internal error(%d): %s',res.statusCode,err.message);
-			
-			return res.json({ 
-				error: 'Server error' 
-			});
-		}
-	});
-});
-*/
-/*      ////////////////
-router.post('/',function(req, res, next) {
- passport.authenticate('bearer',{session: false}, function(err, user, info){
-	console.log('own cb was called  err    ' + err);
-	console.log('user    ' + user);
-	console.log('info      ' + info);
-    //if (err) { return next(err); }
-  //  if (!user) { return res.redirect('/'); }
-
-    // req / res held in closure
-
-  })(req, res, next);
-});
-*/
-
-
 router.post('/', checkToken,
-
-//passport.authenticate('bearer', { session: false })
 
 function(req, res) {
 //router.post('/', function(req, res) {
 	console.log('routes post called');
-	//console.log(util.inspect(req.body.data, {showHidden: false, depth: null}));
-	//console.log('attributes' + attributes['stored-start-city);
-		
+	
 	User.findById(req.user['_id'], function(err, user) {
 		if(err){
 			res.statusCode = 500;
@@ -284,17 +232,7 @@ function(req, res) {
 
 router.delete('/:id', checkToken,
 
-
-//passport.authenticate('bearer', { session: false }), 
-
 function(req, res) {
-//router.delete('/:id',  function(req, res) {
-console.log('access_token        ' + req.body.access_token);
-//console.log('mydata        ' + req.body.mydata);
-//console.log(util.inspect(req.body, {showHidden: false, depth: null}));
-
-
-
 	console.log('routes DELETE was called');
 	//var user = req.user;
 	console.log(util.inspect(req.user, {showHidden: false, depth: null}));
@@ -365,76 +303,5 @@ console.log('access_token        ' + req.body.access_token);
 	});
 
 });
-/*
-router.get('/:id', passport.authenticate('bearer', { session: false }), function(req, res) {
-	
-	Article.findById(req.params.id, function (err, article) {
-		
-		if(!article) {
-			res.statusCode = 404;
-			
-			return res.json({ 
-				error: 'Not found' 
-			});
-		}
-		
-		if (!err) {
-			return res.json({ 
-				status: 'OK', 
-				article:article 
-			});
-		} else {
-			res.statusCode = 500;
-			log.error('Internal error(%d): %s',res.statusCode,err.message);
-			
-			return res.json({ 
-				error: 'Server error' 
-			});
-		}
-	});
-});
 
-router.put('/:id', passport.authenticate('bearer', { session: false }), function (req, res){
-	var articleId = req.params.id;
-
-	Article.findById(articleId, function (err, article) {
-		if(!article) {
-			res.statusCode = 404;
-			log.error('Article with id: %s Not Found', articleId);
-			return res.json({ 
-				error: 'Not found' 
-			});
-		}
-
-		article.title = req.body.title;
-		article.description = req.body.description;
-		article.author = req.body.author;
-		article.images = req.body.images;
-		
-		article.save(function (err) {
-			if (!err) {
-				log.info("Article with id: %s updated", article.id);
-				return res.json({ 
-					status: 'OK', 
-					article:article 
-				});
-			} else {
-				if(err.name === 'ValidationError') {
-					res.statusCode = 400;
-					return res.json({ 
-						error: 'Validation error' 
-					});
-				} else {
-					res.statusCode = 500;
-					
-					return res.json({ 
-						error: 'Server error' 
-					});
-				}
-				log.error('Internal error (%d): %s', res.statusCode, err.message);
-			}
-		});
-	});
-});
-*/
 module.exports = router;
